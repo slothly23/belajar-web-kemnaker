2a646489b0d39e21d1dc8acfe5921ed0dabf90f4
unknown <you@example.com>
2025-10-22 13:14:33 +0700

    latihan json

---
+++ JS/jobsheet/latihan-kompilasi/kompilasi.js
@@ -0,0 +1,12 @@
+let input = 5;
+
+try {
+    let angka = parseInt(input);
+    if (isNaN(angka)) {
+        throw new Error("Input harus berupa angka!");
+    }
+
+    console.log("Kuadrat:", angka * angka);
+} catch (e) {
+    console.error("Terjadi kesalahan:", e.message);
+}
