4acbe7e96f65d7036ce33ad2f4e736439f72dbfc
unknown <you@example.com>
2025-10-22 13:58:37 +0700

    menampilkan error dengan trycatch

---
+++ JS/tugas_praktek/tryCatch.js
@@ -0,0 +1,25 @@
+function bagiAngka(a, b) {
+    let hasil = a / b;
+
+    // tampilkan error jika b = 0
+    try {
+        if (b == 0) {
+            throw new Error("Tidak bisa membagi dengan nol!");
+        }
+        console.log("Hasil pembagian :", hasil);
+    } catch (error) {
+        console.error("Terjadi kesalahan:", error.message); 
+    }
+}
+
+let angka1 = 10;
+let angka2 = 2;
+
+// tampilkan error jika ada variabel yang tidak dideklarasi
+try {
+    bagiAngka(angka1, angka3);
+} catch (error) {
+    console.error("Terjadi kesalahan lain:", error.message);   
+}
+
+console.log("Program selesai.");
