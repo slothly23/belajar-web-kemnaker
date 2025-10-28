6382a2bea2608894c753b44aa5f21c03e76ee929
unknown <you@example.com>
2025-10-28 14:51:17 +0700

    class kombinasi properti + method

--- belajarPBO/method.js
+++ belajarPBO/method.js
@@ -24,3 +24,30 @@ class Car2 {
 // instantiate
 var mobil2 = new Car2();
 console.log(mobil2.sound("Haiii"));
+
+
+// class dengan percabangan
+// identifikasi kekuatan baterai laptop dengan menggunakan percabangan (if, switch)
+
+// class kombinasi properti + method
+class Laptop {
+    constructor(merk, baterai){
+        this.merk = merk;
+        this.baterai = baterai;
+    }
+    // method
+    // perilaku methodnya adalah percabangan
+    cekBaterai(){
+        if (this.baterai >= 80) {
+            console.log(`${this.merk} : Baterai Penuh (${this.baterai}%)`);
+        } else if (this.baterai >= 30) {
+            console.log(`${this.merk} : Baterai Sedang (${this.baterai}%)`);
+        } else {
+            console.log(`${this.merk} : Baterai Lemah (${this.baterai}%)`);
+        }
+    }
+}
+
+// instantiate
+const laptop1 = new Laptop("Asus", 90);
+laptop1.cekBaterai();
