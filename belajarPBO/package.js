284d8e477e90db619751793acb674a12b5d54794
unknown <you@example.com>
2025-10-29 14:51:23 +0700

    module export, class dan instantiate object di file yang berbeda

---
+++ belajarPBO/package.js
@@ -0,0 +1,4 @@
+const Car = require('./vehicle.js')
+
+var mobil1 = new Car("Toyota");
+mobil1.info();
---
+++ belajarPBO/vehicle.js
@@ -0,0 +1,12 @@
+class Car {
+    constructor(brand) {
+        this.brand = brand;
+    }
+    info() {
+        console.log(`${this,this.brand} tes`);
+        
+    }
+}
+
+
+module.exports = Car;
