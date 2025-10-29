0e3b6c188339c718b08e5d058d319e9232797d46
unknown <you@example.com>
2025-10-29 08:51:43 +0700

    getter setter untuk validasi score

--- belajarPBO/getter_setter.js
+++ belajarPBO/getter_setter.js
@@ -26,7 +26,56 @@ orang1.nama = "Ni"
 orang1.nama = "Yiswi"
 // menampilkan yiswi harus pake console karena di * tiadak ada console
 console.log(orang1.nama);
+console.log("=========================================");
 
 
 // latihan getter - setter
 // yg method cekNilai() atur agar ga bisa input nilai selain 0-100
+
+class Peserta {
+    constructor(nama, umur, proglat, nilai) {
+        this.nama = nama;
+        this.umur = umur;
+        this.proglat = proglat;
+        this.score = nilai; // gunakan setter agar validasi jalan
+    }
+
+    get score() {
+        return this._nilai;
+    }
+
+    set score(value) {
+        if (typeof value !== 'number' || value < 0 || value > 100) {
+            console.log("Nilai harus berupa angka di antara 0 dan 100");
+            this._nilai = 0; // default ke 0 jika invalid
+            return;
+        }
+        this._nilai = value;
+    }
+
+    cekNilai() {
+        let ket = ""
+
+        if (this._nilai >= 90) {
+            ket = "Sangat Baik";
+        } else if (this._nilai >= 80) {
+            ket = "Baik";
+        } else if (this._nilai >= 70) {
+            ket = "Cukup";
+        } else if (this._nilai >= 60) {
+            ket = "Kurang";
+        } else {
+            ket = "Tidak Lulus";
+
+        }
+
+        console.log(`Nama : ${this.nama}, Nilai : ${this._nilai}, Keterangan: ${ket}`);
+    }
+}
+
+// instantiate
+var peserta1 = new Peserta('Daisy', 20, 'pbl Web', 190); // otomatis ditolak dan diset ke 0
+
+// tampilkan
+peserta1.cekNilai();
+
