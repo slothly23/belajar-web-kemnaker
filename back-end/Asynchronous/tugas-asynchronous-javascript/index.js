c3470de05022e324566d05addb571257b31bdea2
unknown <you@example.com>
2025-10-30 14:28:08 +0700

    perbaiki pemanggilan callback menggunakan rekursi, biar ga nulis manual satu-satu

--- back-end/Asynchronous/tugas-asynchronous-javascript/index.js
+++ back-end/Asynchronous/tugas-asynchronous-javascript/index.js
@@ -5,16 +5,30 @@ var books = [
     {name: 'Fidas', timeSpent: 2000},
     {name: 'Kalkulus', timeSpent: 4000},
     {name: 'Komik', timeSpent: 1000},
+    {name: 'novel', timeSpent: 1000}
+
 ]
 
 
 
-readBooks(10000, books[0], function(sisaWaktu1) {
-  readBooks(sisaWaktu1, books[1], function(sisaWaktu2) {
-    readBooks(sisaWaktu2, books[2], function(sisaWaktu3) {
-      readBooks(sisaWaktu3, books[3], function(sisaWaktu4) {
-        console.log("Semua buku selesai dibaca atau waktu habis!");
-      });
+// readBooks(10000, books[0], function(sisaWaktu1) {
+//   readBooks(sisaWaktu1, books[1], function(sisaWaktu2) {
+//     readBooks(sisaWaktu2, books[2], function(sisaWaktu3) {
+//       readBooks(sisaWaktu3, books[3], function(sisaWaktu4) {
+//         console.log("Semua buku selesai dibaca atau waktu habis!");
+//       });
+//     });
+//   });
+// });
+
+
+function readAllBooks(index, time) {
+  if (index < books.length) {
+    readBooks(time, books[index], function (sisaWaktu) {
+      // lanjut ke buku berikutnya pakai sisa waktu
+      readAllBooks(index + 1, sisaWaktu);
     });
-  });
-});
+  } 
+}
+
+readAllBooks(0, 10000);
