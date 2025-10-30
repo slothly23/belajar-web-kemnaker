ecd1232143fc9dba2ab179195a0e974475a4360a
unknown <you@example.com>
2025-10-30 16:22:53 +0700

    jawaban soal nomor 4, sudah okee ada opsi cara yg berbeda dengan setiap kondisi bisa pake fungsi async yg beda-beda

--- back-end/Asynchronous/tugas-asynchronous-javascript/soal 4/index4.js
+++ back-end/Asynchronous/tugas-asynchronous-javascript/soal 4/index4.js
@@ -1,34 +1,47 @@
-var filterCarsPromise = require('../promise3.js');
+var filterCarsPromise = require('./promise3.js');
 
+// cari mobil hitam tahun 2019
 filterCarsPromise("black", 2019)
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
+    .then((result) => {
+        console.log("mobil hitam tahun 2019: ", result)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
+// cari mobil silver tahun 2017
 filterCarsPromise("silver", 2017)
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
+    .then((result) => {
+        console.log("mobil silver tahun 2017", result)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
-async function checCars() {
+async function checkCars() {
+    // cari mobil abu-abu tahun 2019
     try {
         const result = await filterCarsPromise("grey", 2019);
-        console.log(result);
+        console.log("mobil abu-abu tahun 2019: ", result);
     } catch (error) {
         console.error(error.message);
     }
 
+    // cari mobil abu-abu tahun 2018
     try {
         const result = await filterCarsPromise("grey", 2018);
-        console.log(result);
+        console.log("mobil abu-abu tahun 2018: ", result);
     } catch (error) {
         console.error(error.message);
     }
 
+    // cari mobil hitam tahun 2020
     try {
         const result = await filterCarsPromise("black", 2020);
-        console.log(result);
+        console.log("mobil hitam tahun 2020: ", result);
     } catch (error) {
         console.error(error.message);
     }
 }
 
-checCars();
+checkCars();
