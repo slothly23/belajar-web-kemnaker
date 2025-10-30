b25deaa21929ced5941412cdddb73520edd992c4
unknown <you@example.com>
2025-10-30 16:09:38 +0700

    jawaban soal nomor 3, sudah okee ada opsi cara yg berbeda

--- back-end/Asynchronous/tugas-asynchronous-javascript/soal 3/index3.js
+++ back-end/Asynchronous/tugas-asynchronous-javascript/soal 3/index3.js
@@ -1,29 +1,34 @@
 var filterBooksPromise = require('./promise2.js');
 
 // menggunakan promise
+// buku berwarna dan jumlah halamannya 50
 filterBooksPromise(true, 50)
-  .then(result => console.log('Hasil:', result))
-  .catch(err => console.error('Error:', err.message));
+  .then((books) => {console.log('buku berwarna dan jumlah halamannya 50: ', books)})
+  .catch((err) => {console.error('Error:', err.message)});
 
 // menggunakan async await
 async function checkBook() {
-    await filterBooksPromise(false, 250);
-}
-
-async function checkBook() {
+  // cari buku hitam putih, jumlah halaman 250
   try {
-    const result1 = await filterBooksPromise(false, 250);
-    console.log("Hasil async/await:", result1);
+    const result = await filterBooksPromise(false, 250);
+    console.log("buku hitam putih, jumlah halaman 250: ", result);
   } catch (err) {
     console.error("Error async/await:", err.message);
   }
 
+  // buku berwarna dan jumlah halamannya 30
+  // buku tidak tersedia
   try {
-    const result2 = await filterBooksPromise(true, 30);
-    console.log("Hasil async/await:", result2);
+    const result = await filterBooksPromise(true, 30);
+    console.log("buku berwarna dan jumlah halamannya 30: ", result);
   } catch (err) {
-    console.error("Error async/await:", err.message);
+    console.error("Error: ", err.message);
   }
 }
+
+// cara lain
+// bisa buat 2 async function terpisah untuk setiap kasus
+  // cariBukuHitamPutih()
+  // cariBukuTidakTersedia()
 
 checkBook();
