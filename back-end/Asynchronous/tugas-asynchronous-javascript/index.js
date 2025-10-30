ae10bae60fcaf3915d1c46fdd4b3a882298ce36d
unknown <you@example.com>
2025-10-30 14:14:03 +0700

    perbaiki pemanggilan callback agar sisa waktu otomatis, ga diinput manual

--- back-end/Asynchronous/tugas-asynchronous-javascript/index.js
+++ back-end/Asynchronous/tugas-asynchronous-javascript/index.js
@@ -8,14 +8,13 @@ var books = [
 ]
 
 
-readBooks(10000, books[0], function() {
-    readBooks(7000, books[1], function() {
-        readBooks(5000, books[2], function() {
-            readBooks(1000, books[3], function() {
-                readBooks(0, books[0], function() {
-                    
-                })
-            })
-        })
-    })
+
+readBooks(10000, books[0], function(sisaWaktu1) {
+  readBooks(sisaWaktu1, books[1], function(sisaWaktu2) {
+    readBooks(sisaWaktu2, books[2], function(sisaWaktu3) {
+      readBooks(sisaWaktu3, books[3], function(sisaWaktu4) {
+        console.log("Semua buku selesai dibaca atau waktu habis!");
+      });
+    });
+  });
 });
--- back-end/Asynchronous/tugas-asynchronous-javascript/index2.js
+++ back-end/Asynchronous/tugas-asynchronous-javascript/index2.js
@@ -8,9 +8,10 @@ var books = [
 ]
 
 readBooksPromise(10000, books[0])
-.then(() => readBooksPromise(7000, books[1])).catch(() => {})
-.then(() => readBooksPromise(5000, books[2])).catch(() => {})
-.then(() => readBooksPromise(1000, books[3])).catch(() => {})
-.then(() => readBooksPromise(0, books[1])).catch(() => {}) // sisa waktu sudah habis
+  .then(sisaWaktu => readBooksPromise(sisaWaktu, books[1]))
+  .then(sisaWaktu => readBooksPromise(sisaWaktu, books[2]))
+  .then(sisaWaktu => readBooksPromise(sisaWaktu, books[3]))
+  .then(sisaWaktu => readBooksPromise(sisaWaktu, books[1])) // waktu tidak cukup untuk baca buku lagi
+  .catch(err => err);
 
 
