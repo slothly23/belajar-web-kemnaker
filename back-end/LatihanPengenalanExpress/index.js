3cc9ad76a0471ebfead41baccbfeb5b3b7d473a1
unknown <you@example.com>
2025-10-31 09:02:51 +0700

    buat route sederhana

--- back-end/LatihanPengenalanExpress/index.js
+++ back-end/LatihanPengenalanExpress/index.js
@@ -4,3 +4,13 @@ const port = 3000;
 
 // middleware global untuk parsing json
 app.use(express.json());
+
+// route sederhana
+app.get('/', (req, res) => {
+    res.send("Hello world dari express");
+})
+
+// menjalankan server di port mana
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
+})
