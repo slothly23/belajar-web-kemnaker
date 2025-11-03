7e315118a01097a9510ea4ac2cc36974a972b0b2
unknown <you@example.com>
2025-11-03 11:55:18 +0700

    membuat router untuk CRUD category

--- back-end/LatORMPrisma/src/routes/movieRoutes.js
+++ back-end/LatORMPrisma/src/routes/movieRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const movieController = require('../controllers/movieContoller');
 const {validationBodyMovies} = require('../middleware/validation'); // validasi jika ada inpuan yg kosong
-const { route } = require('../../../tugas-pengenalan express/Routes/data-orang');
 
 router.get('/', movieController.getAllMovies);
 router.get('/:id', movieController.getMovieById);
