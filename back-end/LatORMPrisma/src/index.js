c68333803b7ec1512e953b0ce241df027e124232
unknown <you@example.com>
2025-11-03 15:11:04 +0700

    resolve error env tidak terbaca, hapus file prisma.config.ts

--- back-end/LatORMPrisma/src/index.js
+++ back-end/LatORMPrisma/src/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express'); // import express
+const cors = require('cors'); // import cors
 
 const app = express();
 
 const movieRoutes =require('./routes/movieRoutes');
 const categoryRoutes =require('./routes/categoryRoutes');
 
-app.use(cors()); // kalau ga pakai cors berarti memperbolehkan akses dari semua domain
+// middleware global
+app.use(cors());  // enable CORS untuk semua origin
+// app.use(cors()); -> aktifkan CORS (default: semua origin diperbolehkan)
+// kalau ga pakai cors berarti memperbolehkan akses dari semua origin
+// berguna saat frontend di origin berbeda
+
+app.use(express.json()); // parse JSON body
+
+// prefix API
+app.use('/api/movies', movieRoutes); // mount route movie di prefix /api/movies
+app.use('/api/categories', categoryRoutes); // mount route category di prefix /api/categories
+
+// health check
+app.get('/', (req, res) => {
+    res.send('API berjalan - gunakan /api/movies dan api/categories');
+});
+
+// jalankan server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server berjalan pada port ${PORT}`);
+})
