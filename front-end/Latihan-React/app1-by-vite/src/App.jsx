de0a31d12a0f80d4db4bda5fe5453cc25f2dec4f
unknown <you@example.com>
2025-11-05 13:58:29 +0700

    nested components, component di dalam component

--- front-end/Latihan-React/app1-by-vite/src/App.jsx
+++ front-end/Latihan-React/app1-by-vite/src/App.jsx
@@ -1,5 +1,6 @@
 import Counter from "./components/Counter";
 import Halo from "./components/Halo" // import component Hallo dari folder components, file Hallo.jsx
+import Profile from "./components/Profile";
 
 function App() {
   // return hanya bisa mengembalika 1 nilai
@@ -11,6 +12,7 @@ function App() {
       <Halo nama="Zalia"/> {/* memanggil component Hallo untuk ditampilkan */}
       <Halo nama="Tech"/> {/* masukin nilai nama */}
       <Counter />
+      <Profile />
     </div>
   )
 }
---
+++ front-end/Latihan-React/app1-by-vite/src/components/Profile.jsx
@@ -0,0 +1,16 @@
+// nested components
+// ada component di dalam component
+function Avatar() {
+    return (
+        <img src="https://cdn-icons-png.freepik.com/512/7077/7077313" alt="avatar" />
+    )
+}
+function Profile() {
+    return (
+        <div>
+            <Avatar /> {/* manggil component Avatar */}
+            <h3>Nama: Zalia</h3>
+        </div>
+    )
+}
+export default Profile;
