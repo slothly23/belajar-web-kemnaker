6e3c2ee9689af9b814082c9aa437b70178d5afe4
unknown <you@example.com>
2025-11-05 15:20:31 +0700

    membuat component card (reuseable)

--- front-end/Latihan-React/latihan-reusable-component/src/App.jsx
+++ front-end/Latihan-React/latihan-reusable-component/src/App.jsx
@@ -1,11 +1,22 @@
 import Button from './components/Button'
+import Card from './components/Card'
 
 function App() {
   return (
     <>
+      <h1>Latihan Reusable Component - Card dan Button</h1>
+      <Card title = "Produk 1">
+        <p>Ini adalah deskripsi produk pertama</p>
+        <Button label="Beli sekarang" color="blue" onClick={() => {alert("Membeli produk 1")}}/>
+      </Card>
+
+      <Card title = "Produk 2">
+        <p>Produk kedua dengan fitur menarik</p>
+        <Button label="Detail" color="gray" onClick={() => {alert("Melihat detail produk 2")}}/>
+      </Card>
+
       <h1>Latihan Reusable Component - Button</h1>
-      {/* saat tombol di klik akan muncul alert */}
-      <Button label="Simpan" color="green" onClick={() => {alert("Data disimpan!")}}/>  
+      <Button label="Simpan" color="green" onClick={() => {alert("Data disimpan!")}}/>  {/* saat tombol di klik akan muncul alert */}
       <Button label="Hapus" color="red" onClick={() => {alert("Data dihapus!")}}/>
       <Button label="Edit" color="orange" onClick={() => {alert("Edit data!")}}/>
     </>
---
+++ front-end/Latihan-React/latihan-reusable-component/src/components/Card.jsx
@@ -0,0 +1,19 @@
+function Card({ title, children }) {
+    const style = {
+        border: "1px solid #ccc",
+        borderRadius: "8px",
+        padding: "16px",
+        margin: "10px",
+        boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)"
+    };
+    
+    return (
+        <div style={style}>
+            <h3>{ title }</h3>
+            <div>
+                { children }
+            </div>
+        </div>
+    )
+}
+export default Card;
