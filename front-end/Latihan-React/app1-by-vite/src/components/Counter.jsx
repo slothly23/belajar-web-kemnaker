7cc3efd1d6dbf51d08583069eb2e5317b858f8a9
unknown <you@example.com>
2025-11-05 14:19:14 +0700

    comment

--- front-end/Latihan-React/app1-by-vite/src/components/Counter.jsx
+++ front-end/Latihan-React/app1-by-vite/src/components/Counter.jsx
@@ -3,15 +3,15 @@ import { useState } from "react";
 // latihan useState
 function Counter() {
     const [count, setCount] = useState(0);
-    // count : variabel
-    // setCount : fungsi untuk menubah nilai awal
-    // useState(0) , 0 adalah nilai awal, dan bisa berubah
+    // count : variabel Tempat data tersimpan
+    // setCount : function untuk menubah data
+    // useState(0) , 0 adalah nilai awal, dan data bisa berubah
 
     return (
         <div>
             <h3>Nilai saat ini: {count}</h3>
-            <button onClick={() => setCount(count + 1)}>Tambah</button> 
             <button onClick={() => setCount(count - 1)}>Kurangi</button>
+            <button onClick={() => setCount(count + 1)}>Tambah</button> 
             {/* event onClick={() => setCount(count + 1)} menjalankan sebuah fungsi ketika di klik */}
         </div>
     )
