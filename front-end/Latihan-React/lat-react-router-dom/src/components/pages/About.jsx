dae91c5f0345c0ae96fda5339b0824c2164fd871
unknown <you@example.com>
2025-11-06 11:05:18 +0700

    buat komponen page, buat navbar pakai bootstrap

---
+++ front-end/Latihan-React/lat-react-router-dom/src/components/pages/About.jsx
@@ -0,0 +1,6 @@
+function About() {
+    return (
+        <h1>ini About</h1>
+    )
+}
+export default About;
---
+++ front-end/Latihan-React/lat-react-router-dom/src/components/pages/Home.jsx
@@ -0,0 +1,6 @@
+function Home() {
+    return (
+        <h1>ini Home</h1>
+    )
+}
+export default Home;
