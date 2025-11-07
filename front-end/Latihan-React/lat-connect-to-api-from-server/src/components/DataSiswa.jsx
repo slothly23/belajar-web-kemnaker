abf363636271f6a7a12b6a0d4007c5788672de09
unknown <you@example.com>
2025-11-07 10:56:30 +0700

    menambahkan bootstrap modal untuk popup tambah siswa

--- front-end/Latihan-React/lat-connect-to-api-from-server/src/components/DataSiswa.jsx
+++ front-end/Latihan-React/lat-connect-to-api-from-server/src/components/DataSiswa.jsx
@@ -23,71 +23,92 @@ export default function DataSiswa() {
     // fungsi untuk pengambilan data, pake axios
     const fetchData = () => {
         axios.get('https://mytechs.my.id/data_siswa_api/apiSiswa.php') // get 'endpoint'
-        .then(response => {
-            // kalau berhasi apa yg dilakukan
-            setStudents(response.data); // masukin data dari API ke array state students | state: wadah
-            console.log('response data: ', response.data); // liat bentuk data
-            // console.log('response: ', response); // liat response
-        })
-        .catch(error => {
-            // kalau gagal apa yg dilakukan
-        })
-        .finally(() => {
-            setLoading(false);
-        })
+            .then(response => {
+                // kalau berhasi apa yg dilakukan
+                setStudents(response.data); // masukin data dari API ke array state students | state: wadah
+                console.log('response data: ', response.data); // liat bentuk data
+                // console.log('response: ', response); // liat response
+            })
+            .catch(error => {
+                // kalau gagal apa yg dilakukan
+            })
+            .finally(() => {
+                setLoading(false);
+            })
     }
 
-    
-   
-  return (
-    <div className="container">
-        <div className="card mt-2">
-            <div className="card-body">
-                <button type="submit" className="btn btn-primary">
-                    Tambah Siswa
-                </button>
 
-                <hr />
-                
-                <table className="table table-bordered table-striped">
-                    <thead className="table-dark">
-                        <tr>
-                            <th scope='col'>#</th>
-                            <th scope='col'>Nama</th>
-                            <th scope='col'>Email</th>
-                            <th scope='col'>Alamat</th>
-                            <th scope='col'>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { students.length === 0 ? (
-                            <tr>
-                                <td colSpan="4" className="text-center text-muted">
-                                    Belum ada data siswa
-                                </td>
-                            </tr>
-                        ) : (
-                            students.map((student, index) => (
-                                <tr key={index}>
-                                    <td>{index + 1}</td>
-                                    <td>{student.nama_siswa}</td>
-                                    <td>{student.email_siswa}</td>
-                                    <td>{student.alamat_siswa}</td>
-                                    <td>
-                                        {/* <button 
-                                            className="btn btn-danger btn-sm"
-                                            onClick={() => onDelete(student.id)}
-                                        >
-                                            Hapus
-                                        </button> */}
-                                    </td>
+
+    return (
+        <>
+            <div className="container">
+                <div className="card mt-2">
+                    <div className="card-body">
+                        {/* tomol modal */}
+                        <button type="button" className="btn btn-primary col-12" data-bs-toggle="modal" data-bs-target="#TambahiswaModal">
+                            Tambah Siswa
+                        </button>
+
+                        <hr />
+
+                        <table className="table table-bordered table-striped">
+                            <thead className="table-dark">
+                                <tr>
+                                    <th scope='col'>#</th>
+                                    <th scope='col'>Nama</th>
+                                    <th scope='col'>Email</th>
+                                    <th scope='col'>Alamat</th>
+                                    <th scope='col'>Aksi</th>
                                 </tr>
-                            ))
-                        ) }
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                                {students.length === 0 ? (
+                                    <tr>
+                                        <td colSpan="4" className="text-center text-muted">
+                                            Belum ada data siswa
+                                        </td>
+                                    </tr>
+                                ) : (
+                                    students.map((student, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{student.nama_siswa}</td>
+                                            <td>{student.email_siswa}</td>
+                                            <td>{student.alamat_siswa}</td>
+                                            <td>
+                                                {/* <button 
+                                                className="btn btn-danger btn-sm"
+                                                onClick={() => onDelete(student.id)}
+                                            >
+                                                Hapus
+                                            </button> */}
+                                            </td>
+                                        </tr>
+                                    ))
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  )
+         
+            <div className="modal fade" id="TambahiswaModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="TambahiswaModal">Modal title</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            ...
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Understood</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
