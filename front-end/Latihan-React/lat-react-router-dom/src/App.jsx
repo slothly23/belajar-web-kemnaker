0f567736e15a65c2f77da095c6d0b5424dfe2d22
unknown <you@example.com>
2025-11-07 08:57:32 +0700

    comment

--- front-end/Latihan-React/lat-react-router-dom/src/App.jsx
+++ front-end/Latihan-React/lat-react-router-dom/src/App.jsx
@@ -7,24 +7,29 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 
 function App() {
+  // deklarasi nilai awal students : array kosong
   const [students, setStudents] = useState([]);
 
+  // ngambil data dari localStorage
   useEffect(() => {
     const savedData = JSON.parse(localStorage.getItem("students")) || [];
     setStudents(savedData);
   }, []);
 
+  // memberikan nilai baru untuk localStorage / input nilai baru
   useEffect(() => {
     localStorage.setItem("students", JSON.stringify(students));
   }, [students]);
 
+  // fungsi untuk menambahkan siswa baru di awal array?, setelahnya adalah data yg sudah ada
   const addStudent = (student) => {
     setStudents([student, ...students]);
   };
 
+  // fungsi untuk hapus 
   const deleteStudent = (id) => {
-    const filtered = students.filter((student) => student.id !== id);
-    setStudents(filtered);
+    const filtered = students.filter((student) => student.id !== id); // pilih semua yang id nya tidak sama dengan id delete
+    setStudents(filtered); // simpan yg pengecualian
   };
 
   return (
