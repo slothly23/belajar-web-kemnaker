a1fa649ef5d858a1c10e3caedc294f5a73a1a060
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2025-11-09 09:54:26 +0700

    hapus auth, belum perlu login

--- full-stack/movie-app/movie-app-be/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const authController = require('../controllers/authController'); // import controller untuk authorization
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
-
